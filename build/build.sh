aaff35911f3465a35cde756f730888acb967ff5e
Ash Kyd <ash@kyd.com.au>
2013-02-18 16:44:25 +1000

    Added form fields, because I'm always using these ones.

---
+++ build/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Don't forget to (sudo) npm install -g less
+lessc ../widgets.less >built.css
---
+++ build/prepare.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -O ../less.js https://raw.github.com/cloudhead/less.js/master/dist/less-1.3.3.min.js
